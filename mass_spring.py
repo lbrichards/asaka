2b4eae8d2597a1f4acce32007d8085d37625853c
Larry Richards <lrichards@iotatx.com>
2021-02-02 16:12:50 +0900

    adding mass spring program

--- mass_spring.py
+++ mass_spring.py
@@ -7,10 +7,11 @@ plt.rcParams['font.size'] = 14
 
 M = 5
 k = 4
-c = .5
+c = 30
 
 ν = c / M
 ω = k / M
+
 
 def f(u, t, ν, ω):
     """
@@ -21,40 +22,48 @@ def f(u, t, ν, ω):
     2) Ma = -kv - cx
     3) let ν = c/M; ω=k/m
     4) a = -νv - ωx
-    5) δv/δt = -νv - ωx = u[1]
-    6) by definition, v = δx/δt = u[0]
-    ∴ δu/δt = f(u) = f(x,v) = [δx/δt, δv/δt] = [v, a]
+    5) δv/δt = -νv - ωx
+    6) by definition, v = δx/δt = u[1]
+    ∴ δu/δt = f(u) = f([x,v]) = [δx/δt, δv/δt] = [v, a]
     """
     v = u[1]
-    a = -ν  * u[1] - ω * u[0]
+    a = -ν * u[1] - ω * u[0]
     return np.array([v, a])
 
+
 t = np.linspace(0, 100, 1000)
-u = odeint(f, (1, .5), t, args=(ν, ω))
+# odeint transforms a differential equation of
+# degree n (n=2 in our case)
+# in one function (x(t) in our case) to
+# a system of differential equations of
+# degree 1 in n functions
+uu = odeint(f, (1, .5), t, args=(ν, ω))
 
 center = t.mean()
 
 fig, ax = plt.subplots()
 ax.grid()
-ax.plot(t, u[:, 0], label=r'$x(t)$ [m]')
-ax.plot(t, u[:, 1], label=r'$v(t)$ [m/s]')
+ax.plot(t, uu[:, 0], label=r'$x(t)$ [m]')
+ax.plot(t, uu[:, 1], label=r'$v(t)$ [m/s]')
 ax.set_xlabel('$t$ [s]')
 ax.legend()
 
-spring = ax.plot([center] * 2, [0, u[0, 0]], 'k', lw=3)[0]
-big_dot = ax.plot(center, u[0, 0], 'ko', ms=20)[0]
-dot1 = ax.plot(t[0], u[0, 0], 'r*')[0]
-dot2 = ax.plot(t[0], u[0, 1], 'g*')[0]
+spring = ax.plot([center] * 2, [0, uu[0, 0]], 'k', lw=3)[0]
+big_dot = ax.plot(center, uu[0, 0], 'ko', ms=20)[0]
+dot1 = ax.plot(t[0], uu[0, 0], 'r*')[0]
+dot2 = ax.plot(t[0], uu[0, 1], 'g*')[0]
+
 
 def animate(i):
     i = np.clip(int(round(i)), 0, len(t) - 1)
     tt = t[i]
-    spring.set_data([center]*2, [0, u[i, 0]])
-    big_dot.set_data([center, u[i, 0]])
-    dot1.set_data([tt, u[i, 0]])
-    dot2.set_data([tt, u[i, 1]])
+    spring.set_data([center] * 2, [0, uu[i, 0]])
+    big_dot.set_data([center, uu[i, 0]])
+    dot1.set_data([tt, uu[i, 0]])
+    dot2.set_data([tt, uu[i, 1]])
 
     fig.canvas.draw_idle()
+
 
 if 0:
     from matplotlib.widgets import Slider
